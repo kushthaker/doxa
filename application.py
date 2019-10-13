395881ffb6f1c069582a30ca5024a80d3e5bd475
kushthaker <kushthaker@gmail.com>
2019-10-13 16:57:42 -0400

    added parent template layout.html

--- application.py
+++ application.py
@@ -1,34 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
+app = application
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+posts = [
+	{
+		'author':'kt',
+		'title':'post 1',
+		'content':'content 1',
+		'date_posted':'April 5 2019'
+	},
+	{
+		'author':'pt',
+		'title':'post 2',
+		'content':'content 2',
+		'date_posted':'April 3 2019'
+	},
+	{
+		'author':'hf',
+		'title':'post 3',
+		'content':'content 3',
+		'date_posted':'April 9 2019'
+	}
+]
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@app.route("/")
+@app.route("/cheese")
+@app.route("/fromage")
+def hello():         
+    return render_template('home.html', posts=posts)
 
-# run the app.
+@app.route("/about")
+def about():
+	return render_template('about.html', title="about")
+
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True         
+    app.run()
+
+
