e751bc7be3f5366919a8b4b7cf95b0731a098a2e
kushthaker <kushthaker@gmail.com>
2019-10-14 16:25:19 -0400

    add User and Post models with SQLAlchemy

--- application.py
+++ application.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
-application = Flask(__name__)
+application = Flask(__name__) # aws eb requires 'application' name for Flask instance
 app = application
+
 app.config['SECRET_KEY'] = '7a273729d601733097ead8f655a410eb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship('Post', backref='author', lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Post('{self.title}','{self.date_posted}')"
+
 
 
 posts = [
@@ -47,11 +74,12 @@ def register():
 @app.route("/login", methods=['GET','POST'])
 def login():
 	form = LoginForm()
-	if form.email.data == 'abc@eee.com' and form.password.data == 'password':
-		flash(f'Login successful for {form.email.data}', 'success')
-		return redirect(url_for('home'))
-	else:
-		flash(f'Login unsuccessful. Please check user and pass', 'danger')
+	if form.validate_on_submit():
+		if form.email.data == 'abc@eee.com' and form.password.data == 'password':
+			flash(f'Login successful for {form.email.data}', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash(f'Login unsuccessful. Please check user and pass', 'danger')
 	return render_template('login.html', title='Login', form=form)
 
 
