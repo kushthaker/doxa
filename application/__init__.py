847adc42866cfb2f252478bbae89a2df700dcf7b
Laurier Mantel <lauriermantel@gmail.com>
2019-10-20 17:05:19 -0400

    working _somewhat_ in production on new doxa-staging env

--- application/__init__.py
+++ application/__init__.py
@@ -3,17 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 application = Flask(__name__) # aws eb requires 'application' name for Flask instance
+application.config['SECRET_KEY'] = '7a273729d601733097ead8f655a410eb'
 
 PG_URL = application.config.get('AWS_RDS_URL')
+if PG_URL is None:
+  PG_URL = os.environ.get('AWS_RDS_URL')
+  if PG_URL is not None:
+    print('USING VARIABLE FROM os.environ')
+else:
+  print('USING VARIABLE FROM application.config ')
+if PG_URL is None:
+  print("USING VARIABLE could_not_find")
 # should be pgmaster:postgres@doxa-database-staging.c8pjbrdeia2z.us-east-1.rds.amazonaws.com
 
-application.config['SECRET_KEY'] = '7a273729d601733097ead8f655a410eb'
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-if PG_URL is not None:
+if PG_URL is None:
   application.config['SQLALCHEMY_DATABASE_URI'] = PG_URL
 else:
   application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/doxa-db-dev'
+
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pgmaster:postgres@doxa-database-staging.c8pjbrdeia2z.us-east-1.rds.amazonaws.com:5432/doxa_db_staging'
 
 db = SQLAlchemy(application)
 from application import routes
--- manage.py
+++ manage.py
@@ -5,9 +5,11 @@ from flask_migrate import Migrate, MigrateCommand
 
 migrate = Migrate(application, db)
 manager = Manager(application)
-manager.add_command('db', MigrateCommand) # python manage.py db __________
+manager.add_command('db', MigrateCommand) # `python manage.py db ___` is the command sequence
 
 if __name__ == '__main__':
   manager.run()
 
 # PostgreSQL version: 11.5 (same in production)
+# https://stackoverflow.com/questions/32798937/cant-migrate-or-upgrade-database-with-flask-migrate-alembic/32803927
+# ^^ useful for deploying new migrations to production
