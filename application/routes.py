4ff7904eeb8453f8ba114cafceccd771dd9c1b98
kushthaker <kushthaker@gmail.com>
2019-10-24 00:43:35 -0400

    add Pillow module to resize uploaded images

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from application import application, db, bcrypt
 from application.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -97,7 +98,10 @@ def save_image(form_picture):
 	_, f_ext = os.path.splitext(form_picture.filename)
 	picture_fn = random_hex + f_ext
 	picture_path = os.path.join(application.root_path, 'static/profile_pics', picture_fn)
-	form_picture.save(picture_path)
+	output_size = (125,125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+	i.save(picture_path)
 	return picture_fn
 
 @application.route("/account", methods=['GET','POST'])
