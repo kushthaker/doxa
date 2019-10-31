cedf44d8d52e191b2fcd460c27f61807bb94749c
kushthaker <kushthaker@gmail.com>
2019-10-31 07:45:23 -0400

    add timed token for reset

--- application/models.py
+++ application/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from application import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from application import db, login_manager, application
 from flask_login import UserMixin
 from datetime import datetime
 
@@ -16,6 +17,19 @@ class User(db.Model, UserMixin):
 	password = db.Column(db.String(60), nullable=False)
 	last_updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
 	posts = db.relationship('Post', backref='author', lazy=True)
+
+	def get_reset_token(self, expires_sec=1800):
+		s = Serializer(application.config['SECRET_KEY'], expires_sec)
+		return s.dumps({'user_id':self.id}).decode('utf-8')
+
+	@staticmethod
+	def verify_reset_token(token):
+		s = Serializer(application.config['SECRET_KEY'])
+		try:
+			user_id = s.loads(token)['user_id']
+		except:
+			return None
+		return User.query.get(user_id)
 
 	def __repr__(self):
 		return "User('%s','%s','%s')" % (self.username, self.email, self.image_file)
