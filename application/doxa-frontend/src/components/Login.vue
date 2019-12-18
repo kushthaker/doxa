4a996bcaab2a9d4b0cdb6e4561b4c261815ac585
Laurier Mantel <lauriermantel@gmail.com>
2019-12-18 16:34:06 -0500

    quick login component fix

---
+++ application/doxa-frontend/src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-form>
+      <input v-model="newUser.email" placeholder="Your email address">
+      <input v-model="newUser.password" placeholder="Password">
+    </v-form>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+  export default {
+    data() {
+      return {}
+    },
+
+    computed: mapState({
+      
+    }),
+
+    beforeMount() {
+      
+    }
+  }
+</script>
