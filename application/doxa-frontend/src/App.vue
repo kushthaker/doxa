5c5775d519f151625f9beb9640cdafaa1bdf668c
Laurier Mantel <lauriermantel@gmail.com>
2020-01-07 23:08:00 -0500

    pushing to GH for review

--- application/doxa-frontend/src/App.vue
+++ application/doxa-frontend/src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Header from '@/components/Header';
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  export default {
+    name: 'App',
+    components: {
+      'app-header': Header
+    }
+  }
 </script>
 
 <style>
-#app {
+
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
--- application/doxa-frontend/src/router/index.js
+++ application/doxa-frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/components/Home'
 import Maestro from '@/components/Maestro'
 import Register from '@/components/Register'
 import Login from '@/components/Login'
+import Settings from '@/components/Settings'
 import store from '@/store'
 
 Vue.use(Router)
@@ -62,13 +63,48 @@ const router = new Router({
       beforeEnter: (to, from, next) => {
         store.dispatch('clearFormErrors')
         store.dispatch('loadCSRF')
-        next() 
+        const isAuthed = store.getters.isAuthenticated;
+        if(isAuthed) {
+          const user = store.getters.currentUser;
+          next(`/maestro/${user.id}`)
+        }
+        else {
+          next()   
+        }
       }
-    }
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      beforeEnter: (to, from, next) => {
+        store.dispatch('clearCredentials').then(() => {
+          next('/login')
+        })
+      }
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: Settings,
+      beforeEnter: (to, from, next) => {
+        store.dispatch('clearFormErrors')
+        store.dispatch('loadCSRF')
+        const isAuthed = store.getters.isAuthenticated;
+        const user = store.getters.currentUser
+        if(isAuthed) {
+          //load user's settings from DB
+          next()
+        }
+        else {
+          next('/')  
+        }
+        
+      }
+    },
   ]
 })
 
-// this just sets the store variable to the currentUser cookie
+// this just sets the store variable "currentUser" to the currentUser cookie
 router.beforeEach((to, from, next) => {
   store.dispatch('checkLogin').then(() => {
     next()
