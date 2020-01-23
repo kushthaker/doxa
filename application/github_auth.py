6a02225d97c94897c9aacde8ebed3c75f84e063a
Callum <mitccall@gmail.com>
2020-01-23 11:53:55 -0500

    cleanup

--- application/github_auth.py
+++ application/github_auth.py
@@ -1,8 +1,8 @@
 from flask import url_for, flash, redirect, request
-from flask_github import GitHub
+from flask_github import GitHub as GitHubCredentials
 from flask_login import login_user, current_user, logout_user, login_required
 
-from github import Github
+from github import Github as GitHubUserData
 
 from application.initialize.config import Config
 from application.app_setup import application
@@ -30,24 +30,24 @@ GITHUB_SCOPES = "user, repo, read:org, read:public_key, gist, notifications, rea
 ###due to how Github-flask's app constructor is written (expects app.config['thing'] specifically)
 #github = GitHub(application)
 
-#Instead: using default GitHub() constructor and then replicating GitHub.init_app(self, app)
+#Instead: using default constructor and then replicating GitHub.init_app(self, app)
 #See https://github.com/cenkalti/github-flask/blob/master/flask_github.py
-github = GitHub()
-github.client_id = Config.GITHUB_CLIENT_ID
-github.client_secret = Config.GITHUB_CLIENT_SECRET
-github.base_url = 'https://api.github.com/'
-github.auth_url = 'https://github.com/login/oauth/'
-github.session = requests.session()
+gitCreds = GitHubCredentials()
+gitCreds.client_id = Config.GITHUB_CLIENT_ID
+gitCreds.client_secret = Config.GITHUB_CLIENT_SECRET
+gitCreds.base_url = 'https://api.github.com/'
+gitCreds.auth_url = 'https://github.com/login/oauth/'
+gitCreds.session = requests.session()
 
 
 #Call to attempt to authorize a user with the GitHub API
 @application.route(GITHUB_AUTH_ROUTE)
 def github_auth_route():
-	return github.authorize(scope=GITHUB_SCOPES)
+	return gitCreds.authorize(scope=GITHUB_SCOPES)
 
 #Invoked after github authorization attempt
 @application.route(GITHUB_CALLBACK_ROUTE)
-@github.authorized_handler
+@gitCreds.authorized_handler
 def authorized(oauth_token):
 	next_url = request.args.get('next') or url_for('home')
 	if oauth_token is None:
@@ -55,5 +55,5 @@ def authorized(oauth_token):
 		return redirect(next_url)
 
 	#TODO: retrieve authenticated user information and update/save to the database
-
+	flash("Authorization successful.")
 	return redirect(next_url)
