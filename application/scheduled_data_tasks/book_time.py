3e030510a90fdcfe973188b0cda8c40ee2ea4c1f
kushthaker <kushthaker@gmail.com>
2020-02-19 16:45:14 -0500

    changes

--- application/scheduled_data_tasks/book_time.py
+++ application/scheduled_data_tasks/book_time.py
@@ -4,7 +4,7 @@ import google_auth_httplib2
 from google.oauth2.credentials import Credentials
 from application.google_auth import get_credentials_dict
 import googleapiclient.discovery
-from dateutil import parser
+import dateutil
 import json
 from google.auth.exceptions import RefreshError
 import pytz
@@ -14,24 +14,23 @@ import pytz
 from application.initialize.db_init import db
 
 
-def set_utc_tz(event, g_user):
-  user_tz = pytz.timezone(g_user.primary_timeZone)
+def set_utc_tz(event):
   if event.get('start').get('dateTime'):
-    s_dt = parser.parse(event.get('start').get('dateTime')).replace(tzinfo=pytz.UTC)
-    s_dt = s_dt.astimezone(user_tz)
-    e_dt = parser.parse(event.get('end').get('dateTime')).replace(tzinfo=pytz.UTC)
-    e_dt = e_dt.astimezone(user_tz)
+    s_dt = dateutil.parser.parse(event.get('start').get('dateTime'))
+    e_dt = dateutil.parser.parse(event.get('end').get('dateTime'))
+    s_dt = s_dt.replace(tzinfo=pytz.UTC) - s_dt.utcoffset()
+    e_dt = e_dt.replace(tzinfo=pytz.UTC) - e_dt.utcoffset()
   elif event.get('start').get('date'):
-    s_dt = parser.parse(event.get('start').get('date')).replace(tzinfo=pytz.UTC)
-    s_dt = s_dt.astimezone(user_tz)
-    e_dt = parser.parse(event.get('end').get('date')).replace(tzinfo=pytz.UTC)
-    e_dt = e_dt.astimezone(user_tz)
+    s_dt = dateutil.parser.parse(event.get('start').get('date'))
+    e_dt = dateutil.parser.parse(event.get('end').get('date'))
   event_id = event.get('id')
   return dict({'start':s_dt,'end':e_dt,'event_id':event_id})
 
 def get_upcoming_events(service):
-  minTime = (pytz.UTC.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=60)).isoformat() 
-  events_result = service.events().list(calendarId='primary', timeMin=minTime, maxResults=200, singleEvents=True, orderBy='startTime').execute()
+  # minTime = (pytz.UTC.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=60)).isoformat() 
+  # events_result = service.events().list(calendarId='primary', timeMin=minTime, maxResults=200, singleEvents=True, orderBy='startTime').execute()
+  now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+  events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=100, singleEvents=True, orderBy='startTime').execute()
   events = events_result.get('items', [])
   if not events:
     print('No upcoming events found.')
@@ -70,7 +69,6 @@ def book_upcoming_week_focus_time():
   Book time into first delta > g_user.user.focus_length over user's next 7 days.
 
   """
-
   g_users = GoogleCalendarUser.query.all()
 
   for g_user in g_users:
@@ -78,70 +76,37 @@ def book_upcoming_week_focus_time():
     if credentials.expired is False and credentials.valid is True: ## other checks here?
       service = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
       events = get_upcoming_events(service)
-      utc_events = [set_utc_tz(e, g_user) for e in events]
+      utc_events = [set_utc_tz(e) for e in events]
       
-      dates = dict.fromkeys(set([e.get('start').date() for e in utc_events]))
+      dates = []
+      for i in range(0,10):
+        dates.append(datetime.datetime.utcnow().date() + datetime.timedelta(days=i))
+      dates = dict.fromkeys([e for e in dates if e.weekday() not in [5,6]])
       for k,v in dates.items():
         dates[k] = [e for e in utc_events if e.get('start').date() == k]
 
-      book_date = datetime.datetime.utcnow().date()
-
-      for i in range(1,8):
-        book_date = book_date + datetime.timedelta(1)
-        workday_start =  pytz.utc.localize(datetime.datetime(book_date.year, book_date.month, book_date.day, user.workday_start.hour, user.workday_start.minute))
-        workday_start = workday_start.astimezone(pytz.timezone(g_user.primary_timeZone))
-        workday_end = pytz.utc.localize(datetime.datetime(book_date.year, book_date.month, book_date.day,user.workday_end.hour, user.workday_end.minute))
-        workday_start = workday_end.astimezone(pytz.timezone(g_user.primary_timeZone))
-        day_events = sorted(dates[book_date], key= lambda i: i['start'])
+      for k,v in dates.items():
+        workday_start =  pytz.utc.localize(datetime.datetime(k.year, k.month, k.day, user.workday_start.hour, user.workday_start.minute))
+        workday_end = pytz.utc.localize(datetime.datetime(k.year, k.month, k.day, user.workday_end.hour, user.workday_end.minute))
+        day_events = sorted(dates[k], key= lambda i: i['start'])
 
         if attempt_booking(workday_start, day_events[0].get('start'), g_user.user.focus_length):
-          dates[book_date].append(dict({'focus_booked': 1}))
+          dates[k].append(dict({'focus_booked': 1}))
           continue
           
         for i in range(len(day_events)-1):
           if attempt_booking(day_events[i].get('end'),day_events[i+1].get('start'), g_user.user.focus_length):
-            dates[book_date].append(dict({'focus_booked': 1}))
+            dates[k].append(dict({'focus_booked': 1}))
             break
         
-        if dates[book_date][-1].get('focused_booked'):
+        if dates[k][-1].get('focus_booked'):
           continue
         else:
           if attempt_booking(day_events[-1].get('end'), workday_end, g_user.user.focus_length):
-            dates[book_date].append(dict({'focus_booked': 1}))
+            dates[k].append(dict({'focus_booked': 1}))
             continue
 
-        dates[book_date].append(dict({'focus_booked': 0}))
-
-
-"""
-How to insert a calendar event
-"""
-
-start = datetime.datetime(2020, 2, 2, 22, 26).isoformat()
-end = start + datetime.timedelta(0, 7200)
-
-event = {
-  'summary': 'Focus Time',
-  'location': 'Desk',
-  'description': 'Uninterrupted time for your most important work.',
-  'start': {
-    'dateTime': datetime.datetime(2020, 2, 2, 22, 26).isoformat(),
-    'timeZone': g_user.primary_timeZone,
-  },
-  'end': {
-    'dateTime': datetime.datetime(2020, 2, 2, 22, 26).isoformat(),
-    'timeZone': g_user.primary_timeZone,
-  },
-  'colorId':'3',
-  'reminders': {
-    'useDefault': False,
-    'overrides': [
-      {'method': 'popup', 'minutes': 10},
-    ],
-  },
-}
-
-event = service.events().insert(calendarId='primary', body=event).execute()
+        dates[k].append(dict({'focus_booked': 0}))
 
 """
 Utility functions for Google API
