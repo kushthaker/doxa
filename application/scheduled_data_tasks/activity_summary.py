a012512a85e2808be8332ee23cd2305fea66d5a5
Laurier Mantel <lauriermantel@gmail.com>
2020-02-22 19:21:49 -0800

    fixing requirements.txt, update job

--- application/scheduled_data_tasks/activity_summary.py
+++ application/scheduled_data_tasks/activity_summary.py
@@ -7,11 +7,17 @@ def update_user_activity_data_rows():
   start_time_utc = du.rounddown_next_5min(dt.datetime.now() - dt.timedelta(days=5))
   end_time_utc = du.rounddown_next_5min(dt.datetime.now() + dt.timedelta(days=5))
   users = User.query.filter(User.fully_authenticated == True).all()
+
   for user in users:
     user_activity_df = du.collaboration_activity_data_for_given_period(user, start_time_utc, end_time_utc)
-    current_user_activity = ActivityReportRow.query.filter((ActivityReportRow.datetime_utc >= start_time_utc) & \
-                                                            (ActivityReportRow.datetime_utc <= end_time_utc)).all()
-    # I don't know if a bulk update would be better given the form of the data ¯\_(ツ)_/¯ 
-    [db.session.delete(cua) for cua in current_user_activity]
+    delete_result = ActivityReportRow.query.filter((ActivityReportRow.datetime_utc >= start_time_utc) & (ActivityReportRow.datetime_utc <= end_time_utc) & (ActivityReportRow.user_id == user.id)).delete()
+    print(delete_result)
     db.session.commit()
-    user_activity_df.to_sql('activity_report_rows', db.engine, index=False, if_exists='append')
+    print('deleted existing activity')
+    # I don't know if a bulk update would be better given the form of the data ¯\_(ツ)_/¯ 
+    print(f'activity DF info for {user.email}:')
+    user_activity_df.info()
+    
+    
+    user_activity_df.to_sql('activity_report_rows', db.engine, index=False, if_exists='append', method='multi')
+    print('updated activity')
