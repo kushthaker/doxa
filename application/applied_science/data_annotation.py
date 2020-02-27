11b3eff391e0569ddf4faf3c796658d466e967e2
Laurier Mantel <lauriermantel@gmail.com>
2020-02-27 02:00:09 -0500

    Disconnect days (#60)
    * disconnect days - needs CSS paging @kushthaker
    
    * Vue app build
    
    * Vue app build with tweaks, bug fix

--- application/applied_science/data_annotation.py
+++ application/applied_science/data_annotation.py
@@ -3,18 +3,34 @@ import application.applied_science.data_utils as du
 from application.models import ActivityReportRow
 
 def labelled_focus_time_df(user, start_datetime=None, end_datetime=None, work_hours_only=True):
-  start_datetime, end_datetime = (user, start_datetime, end_datetime) if (start_datetime and end_datetime) else du.current_user_week_limits(user)
+  '''
+    Core method to deliver reliable, annotated data to users
+  '''
+  start_datetime, end_datetime = (start_datetime, end_datetime) if (start_datetime and end_datetime) else du.current_user_week_limits(user)
   hour_timezone_offset = user.timezone_offset_hours
   date_filter = (ActivityReportRow.datetime_utc >= start_datetime) & (ActivityReportRow.datetime_utc < end_datetime)
   user_filter = ActivityReportRow.user_id == user.id
-  activity_df = pd.read_sql(ActivityReportRow.query.filter(date_filter & user_filter).statement, ActivityReportRow.query.session.bind)
+  activity_df = pd.read_sql(ActivityReportRow.query.filter(date_filter & user_filter) \
+                            .order_by(ActivityReportRow.datetime_utc.asc()).statement, \
+                            ActivityReportRow.query.session.bind)
   activity_df.index = activity_df.datetime_utc
-  if work_hours_only:
-    activity_df = du.workday_weekend_filter(activity_df, hour_timezone_offset=hour_timezone_offset)
-    activity_df = du.workday_time_filter(activity_df, hour_timezone_offset=hour_timezone_offset)
-  activity_df['is_collaborative'] = naive_collaborative_time_series(activity_df)
-  activity_df['is_focused'] = activity_df['is_collaborative'] == False
+ 
+  activity_df = label_focus_time(activity_df)
+  activity_df = apply_work_hours_label(activity_df, hour_timezone_offset)
+  activity_df.index += du.dt.timedelta(hours=hour_timezone_offset)
+  activity_df.datetime_utc = activity_df.index
   return activity_df
+
+def label_focus_time(df):
+  df['is_collaborative'] = naive_collaborative_time_series(df)
+  df['is_focused'] = df['is_collaborative'] == False
+  return df
+
+def apply_work_hours_label(df, hour_timezone_offset):
+  weekend_filter = du.workday_weekend_filter(df, hour_timezone_offset=hour_timezone_offset, return_filter=True)
+  workday_filter = du.workday_time_filter(df, hour_timezone_offset=hour_timezone_offset, return_filter=True)
+  df['not_work_hours'] = ((weekend_filter) & (workday_filter == False)) | (weekend_filter == False)
+  return df
 
 # Naive focused time calculation
 NAIVE_STREAK_LENGTH = 1
@@ -24,9 +40,7 @@ MINIMUM_INTERRUPTION_SEND_AMOUNT = 1
 MINIMUM_INTERRUPTION_READ_AMOUNT = 1
 
 def naive_collaborative_time_series(df):
-    return ((df[du.SLACK_CONVERSATION_READ_COLUMN_NAME] >= 1) | \
-    (df[du.SLACK_USER_EVENT_COLUMN_NAME] >= 1) | \
-    (df[du.GOOGLE_CALENDAR_EVENT_COUNT_COLUMN_NAME] >= 1))
+  return ((df[du.SLACK_CONVERSATION_READ_COLUMN_NAME] >= 1) | (df[du.SLACK_USER_EVENT_COLUMN_NAME] >= 1) | (df[du.GOOGLE_CALENDAR_EVENT_COUNT_COLUMN_NAME] >= 1))
 
 # can likely change these arguments to kwargs later to make this stuff more generalizable
 def is_collaborative_time(df, min_interrupt_len=1, \
---
+++ application/doxa-frontend/src/components/DisconnectedTime.vue
@@ -0,0 +1,62 @@
+<template>
+  <span>
+    <h3 class="text-center">Disconnecting after hours</h3>
+    <row>
+      <h4> You have disconnected {{collaborativeDayCounts.disconnectedDays}} out of {{collaborativeDayCounts.totalDays}} potential days.
+      </h4>
+    </row>
+  </span>
+</template>
+
+<script>
+  export default {
+    props: {
+      activity: {
+        default: Array(),
+        type: Array
+      }
+    },
+    computed: {
+      collaborativeDayCounts: function () {
+        var activity = this.activity
+        var disconnectedDays = 0
+        var offHourActivities = activity.filter(function(act) {
+          return act.not_work_hours
+        })
+        var uniqueDays = new Set(offHourActivities.map(function(act) {
+          return new Date(act.datetime_utc).getUTCDate()
+        }))
+        var days = {}
+        var today = new Date().getDate()
+        uniqueDays.forEach(function(day) {
+          if (day <= today) {
+            days[day] = 0  
+          }
+        })
+        offHourActivities.forEach(function(act) {
+          if(act.is_collaborative) {
+            var day = new Date(act.datetime_utc).getUTCDate()
+            if(day in days) {
+              days[day] += 1
+            }
+          }
+        })
+        var disconnectedDays = 0
+        var totalDays = Object.keys(days).length
+        Object.keys(days).forEach(function(day) {
+          if(days[day] == 0) {
+            disconnectedDays += 1
+          }
+        })
+        return {
+          totalDays,
+          disconnectedDays
+        }
+      }
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
